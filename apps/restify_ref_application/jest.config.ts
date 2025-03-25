d95e2e1b24524c70f068745c2b6f38cf465cd325
Jorge Ordoñez <jorgeorm@gmail.com>
2025-03-25 10:17:06 -0500

    feat(restify_ref_application): update dependencies in package.json
    - Added @nx/esbuild and @swc/jest for improved build and testing support.
    - Included esbuild and jest-environment-node for enhanced performance and compatibility.

---
+++ apps/restify_ref_application/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'RestifyReferenceApplication',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': '@swc/jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/restify_ref_application',
+};
